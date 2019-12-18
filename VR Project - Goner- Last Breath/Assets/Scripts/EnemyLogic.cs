59bcde49e8a57aa1d725dc05d43da080ef5e2a84
Mor Ohana <Soramora1@Gmail.com>
2019-12-18 18:40:41 +0200

    Switched from PlayerController to CameraRig.
    In the game the player will not be able to move.

--- VR Project - Goner- Last Breath/Assets/Scripts/EnemyLogic.cs
+++ VR Project - Goner- Last Breath/Assets/Scripts/EnemyLogic.cs
@@ -1,24 +1,48 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyLogic : MonoBehaviour
 {
     [SerializeField]
     private Transform playerLocation;
+    
+    [Header("Enemy properties")]
+    private float runningSpeed = 2f;
+    private bool isZombieDead;
 
+    [Space]
+
+    [Header("Audio variables")]
+    private AudioSource audioSource;
     [SerializeField]
-    private float runningSpeed;
+    private AudioClip zombieDead;
+
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
 
     private void Update()
     {
         transform.LookAt(playerLocation);
 
-        transform.position = transform.position + transform.forward * Time.deltaTime * runningSpeed;
+        if (!isZombieDead)
+        {
+            transform.position = transform.position + transform.forward * Time.deltaTime * runningSpeed;
+        }
+        else
+        {
+            transform.position = transform.position - transform.up * Time.deltaTime;
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        Destroy(gameObject);
+        isZombieDead = true;
+        gameObject.GetComponent<Collider>().isTrigger = true;
+        audioSource.Stop();
+        audioSource.volume = 0.4f;
+        audioSource.PlayOneShot(zombieDead);
+
+        Destroy(gameObject, 2.3f);
     }
 }
