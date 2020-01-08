6de232f950ac7c7df6ece37a031c8040c7f8193b
RoeiRubach <rrubach@gmail.com>
2020-01-08 17:51:09 +0200

    Added a "camera gaze", "switching scenes" and modified the rope

---
+++ VR Project - Goner- Last Breath/Assets/CameraGaze.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class CameraGaze : MonoBehaviour
+{
+    public LayerMask layerMask;
+
+    [SerializeField]
+    private ControlByRope controlByRope;
+
+    private void Update()
+    {
+        Ray myRay = new Ray(transform.position, transform.TransformDirection(Vector3.forward));
+
+        RaycastHit myHit;
+        if (Physics.Raycast(myRay, out myHit, Mathf.Infinity, layerMask))
+        {
+            controlByRope.isLookingAtDummy = true;
+        }
+        else
+        {
+            controlByRope.isLookingAtDummy = false;
+        }
+
+    }
+}
